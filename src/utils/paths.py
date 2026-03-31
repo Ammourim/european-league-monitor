ff74de5ebe24fc137de3864301bb7a0024a54c89
alan <alanamorim28@gmail.com>
2026-03-31 02:26:30 -0300

    feat: criando arquivos de paths

---
+++ src/utils/paths.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+# raiz do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# camadas
+DATA_DIR = BASE_DIR / "data"
+
+BRONZE_DIR = DATA_DIR / "bronze"
+SILVER_DIR = DATA_DIR / "silver"
+
+# datasets específicos
+BRONZE_COMPETITIONS = BRONZE_DIR / "competitions"
+SILVER_COMPETITIONS = SILVER_DIR / "competitions"
+
+
+BRONZE_MATCHES = BRONZE_DIR / "matches_by_competition"
+
+
+BRONZE_SCORERS = BRONZE_DIR / "scorers_by_competition"
+
+
+BRONZE_STANDINGS = BRONZE_DIR / "standings_by_competition"
+
+
+BRONZE_TEAMS = BRONZE_DIR / "teams_by_competition"
