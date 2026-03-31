8255273533c63210fe5f2b1c566b2c6f0ffd50bd
alan <alanamorim28@gmail.com>
2026-03-31 01:34:01 -0300

    feat: etapa de extracao completa

---
+++ src/extracts/scorers_by_competition.py
@@ -0,0 +1,51 @@
+#%%
+import os
+import requests
+from pathlib import Path
+from dotenv import load_dotenv
+import json
+import logging
+import time
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+LIGAS = ['PL', 'PD', 'BL1', 'CL', 'FL1', 'SA', 'BSA', 'WC'] 
+
+API_KEY=os.getenv('API_KEY')
+headers = { 'X-Auth-Token': API_KEY }
+
+
+bronze = Path(__file__) / '..' / '..'/ '..' / 'data' / 'bronze' / 'scorers_by_competition'
+bronze.mkdir(parents=True, exist_ok=True)
+
+
+def extract_scorers():
+    logger.info(f'Extraindo scorers de {len(LIGAS)} ligas!')
+    
+    for liga in LIGAS:
+        logger.info(f'Extraindo scorers da liga: {liga}')
+        URL = f'http://api.football-data.org/v4/competitions/{liga}/scorers'
+        caminho = bronze / f'scorers-{liga}.json'
+
+        try:
+            response = requests.get(f'{URL}', headers=headers, timeout=10)
+            response.raise_for_status()
+            dados = response.json()
+
+            with open(caminho, 'w', encoding='utf-8') as f:
+                json.dump(dados, f, indent=4)
+
+            logger.info(f'scorers da liga {liga} salvos com sucesso!')
+            
+        except requests.HTTPError as e:
+            logger.error(f'Erro ao extrair scorers da liga {liga}: {e}')
+        time.sleep(2)
+        logger.info(f'Extracao finalizada!')
+
+extract_scorers()
